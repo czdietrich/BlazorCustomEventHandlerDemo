b309dc0f9a723ca7a9426b1ae0611b19b37211e3
czdietrich <43068830+czdietrich@users.noreply.github.com>
2022-03-25 14:55:42 +0100

    add simple demo code for show casing custom events in Blazor

---
+++ BlazorCustomEventHandlerDemo/EventHandlers.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorCustomEventHandlerDemo;
+
+using Microsoft.AspNetCore.Components;
+
+[EventHandler( "onmy-event", typeof( MyEventArgs ))]
+public static class EventHandlers
+{
+}
+
+public class MyEventArgs : EventArgs
+{
+    public string? Value { get; set; }
+}
